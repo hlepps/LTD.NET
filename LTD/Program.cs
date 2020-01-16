908b6cbc4076c7c285d1ce0fec767ebf5c13239c
hlepps <pomapiotrek@gmail.com>
2020-01-16 15:14:12 +0100

    stworzenie projektu

---
+++ LTD/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Window;
+using SFML.Graphics;
+namespace LTD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
